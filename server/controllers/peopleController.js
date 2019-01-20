dcf0c4f631f2710174ae7b4eebbde29134032359
Giulio Starace <giulio.starace@gmail.com>
2019-01-20 18:57:13 +0000

    continue setting up for RESTful API

---
+++ server/controllers/peopleController.js
@@ -0,0 +1,13 @@
+const peopleController = {};
+
+peopleController.getPeople = (req, res) => {
+  res.send({message: "hello"});
+};
+peopleController.getPerson = (req, res) => {
+  res.send({message: "hello"});
+};
+peopleController.postPerson = (req, res) => {
+  res.send({message: "hello"});
+};
+
+export default peopleController
--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,16 @@
 // file for handling routes
 
 //<section>~~~~~~~~~~~~~~~~~~~~~Imports~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import express from 'express'
+import { Router } from 'express'
 import basicController from './controllers/basicController'
+import peopleController from './controllers/peopleController'
 //</section> End of imports
 
-const routes = express();
+const routes = Router();
 
-routes.get('/', basicController.get);
+routes.get('/', basicController.getHome);
+routes.get('/people', peopleController.getPeople);
+routes.get('/people/:userid', peopleController.getPerson);
+routes.post('/people', peopleController.postPerson);
 
 export default routes;
