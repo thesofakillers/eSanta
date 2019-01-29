06f315984580fc9889abb3cac51bd2dfe7feeacf
Giulio Starace <giulio.starace@gmail.com>
2019-01-29 00:20:37 +0000

    add authorization middleware

---
+++ server/middleware/authorize.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  // try/catch block tries to verify token, handling failure to do so
+  try {
+    // parse the token from the headers
+    const receivedToken = req.headers.authorization.split(" ")[1];
+    console.log(receivedToken);
+    // try to verify token
+    const verifiedToken = jwt.verify(receivedToken, process.env.JWT_KEY);
+    // set a custom request attribute corresponding to the token
+    req.jwt = verifiedToken;
+    // tell express to move on to the next function
+    next();
+  } catch (error) { // error raised when token is invalid
+    return res.status(403).send({message: 'Authorization Failed'})
+  }
+}
--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ const wishlistController = require('./controllers/wishlistController');
 const registerController = require('./controllers/registerController');
 const loginController = require('./controllers/loginController');
 const userController = require('./controllers/userController');
+const authorize = require('./middleware/authorize');
 //</section> End of imports
 
 const routes = Router();
@@ -26,11 +27,13 @@ routes.post('/people', peopleController.postPerson);
 // only admin can see all users on website (handled with middleware + internally)
 routes.get('/users', userController.getUsers)
 
-// anyone can get wishlists
-routes.get('/wishlists', wishlistController.getWishlists);
-routes.get('/wishlists/:username', wishlistController.getWishlist);
-// only :username can post to /wishlists/:username (handled with middleware)
-routes.post('/wishlists/:username', wishlistController.createWishlist);
+// Only admin can see all wishlists
+routes.get('/wishlists', authorize, wishlistController.getWishlists);
+// Only username (+admin) can see /wishlists/:username
+routes.get('/wishlists/:username', authorize, wishlistController.getWishlist);
+// only users can post to /wishlists (handled with middleware)
+routes.post('/wishlists', authorize, wishlistController.createWishlist);
+// only username can edit /wishlists/:username (handled with middleware)
 routes.put('/wishlists/:username', wishlistController.editWishlist);
 
 // anyone can try registering
