f1dc874a6fe1538d56b49f5e3c12c418c17c19d6
Giulio Starace <giulio.starace@gmail.com>
2019-01-30 20:36:03 +0000

    add better feedback to user

--- public/scripts/wishlistHandler.js
+++ public/scripts/wishlistHandler.js
@@ -11,12 +11,15 @@ $(document).ready(function() {
       },
       statusCode : {
         403: function(){
-          $("#mainDynamic").html('<h1>You are not authorized</h1>')
+          $("#mainDynamic").html("\
+          <h1>You are not authorized</h1>\
+          <p class='text-justify pt-3'>Given a specific wishlist, only its owner and Santa's\
+          elves are allowed access.</p>")
         }
       },
       success : function(response){console.log("yay")}
     })
   });
 
-  
+
 });
