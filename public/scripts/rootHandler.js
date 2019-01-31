2432a7b996dd75d9131356e85d820b59702f5ebf
Giulio Starace <giulio.starace@gmail.com>
2019-01-31 21:08:15 +0000

    add comments and better messages to client

--- public/scripts/rootHandler.js
+++ public/scripts/rootHandler.js
@@ -2,13 +2,20 @@ $(document).ready(function() {
   var main = $("#mainDynamic");
   var navBar = $("#siteNav");
   $('.logo.clickable').on('click', function() {
+    // determine whether user is currently logged in
     var loginStatus = JSON.parse(localStorage.getItem('loggedIn'));
+    // if user is currently logged in
     if (loginStatus) {
+      // get their JWT
       var currToken = localStorage.getItem('Authorization')
+      // parse it
       var decodedCurrToken = jwt_decode(currToken.split(' ')[1]);
+      // determine whether the user is an admin (elf)
       var adminStatus = decodedCurrToken.access_token
 
+      // if the user is an admin
       if (adminStatus){
+        // render POST to /people and GET /users options
         main.html("\
           <div class='row'>\
             <div class='col labeled-button text-center'>\
@@ -24,10 +31,11 @@ $(document).ready(function() {
           <div class='row pt-4'>\
               <div class='col-1'></div>\
               <div class='col text-justify lead'>\
-                Ho ho ho! It's about time! Santa has finally modernized and hit the web.\
-                Register now to create and edit your very own Christmas wishlist so\
-                that Santa and his elves can easily have access to it.\
-                Be careful though...you may be on the naughty list.\
+                Alabaster? Is that you? Or is it Sugarplum Mary?\
+                ...Anyway, as one of my lovely elves, you have the ability\
+                to add people to the Naughty List, browse our registered users \
+                and view their wishlists.\
+                Thank you so much for your work and Merry Christmas!.\
               </div>\
               <div class='col-1'></div>\
           </div>\
@@ -37,7 +45,8 @@ $(document).ready(function() {
         <div class='nav-item nav-link clickable naughtyPost'>Add to Naughty List</div>\
         <div class='nav-item nav-link clickable userList'>Users</div>\
         ");
-      } else {
+      } else { // if the user is not an admin
+        // render POST/PUT /wishlist:user and GET naughtyList buttons
         main.html("\
           <div class='row'>\
             <div class='col labeled-button text-center'>\
@@ -53,10 +62,10 @@ $(document).ready(function() {
           <div class='row pt-4'>\
               <div class='col-1'></div>\
               <div class='col text-justify lead'>\
-                Ho ho ho! It's about time! Santa has finally modernized and hit the web.\
-                Register now to create and edit your very own Christmas wishlist so\
-                that Santa and his elves can easily have access to it.\
-                Be careful though...you may be on the naughty list.\
+                Now who do we have here? Have you been naughty? Nice?\
+                Why don't you go and check on my official Naughty List?\
+                If you're sure you've been nice though, what are you waiting\
+                for!? Go and set up your wishlist! And Merry Christmas!\
               </div>\
               <div class='col-1'></div>\
           </div>\
@@ -66,7 +75,8 @@ $(document).ready(function() {
         <div class='nav-item nav-link clickable ownWishlist'>My Wishlist</div>\
         ");
       }
-    } else {
+    } else { // if the client is not logged in
+      // render GET naughty list button
       main.html("\
         <div class='row'>\
           <div class='col labeled-button text-center'>\
