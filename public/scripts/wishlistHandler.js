df4c4bae0209d663425dc23e2363662721febf86
Giulio Starace <giulio.starace@gmail.com>
2019-01-31 13:31:32 +0000

    start setting up view your own wishlist route

--- public/scripts/wishlistHandler.js
+++ public/scripts/wishlistHandler.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  $("#mainDynamic").on('click', ".userWishlist.clickable", function(){
+
+  main = $("#mainDynamic")
+  main.on('click', ".userWishlist.clickable", function(){
     var currToken = localStorage.getItem('Authorization')
     var userRequested = $(this).data('username')
     $.ajax({
@@ -10,7 +12,7 @@ $(document).ready(function() {
       },
       statusCode : {
         403: function(){
-          $("#mainDynamic").html("\
+          main.html("\
           <h1>You are not authorized</h1>\
           <p class='text-justify pt-3'>Given a specific wishlist, only its owner and Santa's\
           elves are allowed access.</p>")
@@ -20,5 +22,38 @@ $(document).ready(function() {
     })
   });
 
+  main.on('click', ".clickable.ownWishlist", function(){
+    var currToken = localStorage.getItem('Authorization')
+    var decodedCurrToken = jwt_decode(currToken.split(' ')[1]);
+    var currentUser = decodedCurrToken.username
+    console.log(currentUser)
+    $.ajax({
+      url: "/wishlists/" + currentUser,
+      contentType: 'application/json',
+      headers: {
+        Authorization: currToken
+      },
+      statusCode : {
+        403: function(response){
+          console.log(response.responseJSON.message)
+          main.prepend("\
+          <div class='alert alert-danger alert-dismissible fade show' role='alert'>\
+          It seems you are not signed in!\
+          <button type='button' class='close' data-dismiss='alert' aria-label='Close'>\
+          <span aria-hidden='true'>&times;</span>\
+          </button>\
+          </div>\
+          ")
+        },
+        400: function(response){
+          createWishlist(currentUser)
+        },
+        200: function(response){
+          console.log(response.message)
+        }
+      }
+    })
+
+  })
 
 });
